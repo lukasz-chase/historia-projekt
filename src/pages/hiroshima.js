f237b8fe9163b633ad9d11b596d9646f5c9564bd
luki752 <luki7522@gmail.com>
2021-03-18 15:39:14 +0100

    added commentary to monuments

--- src/pages/hiroshima.js
+++ src/pages/hiroshima.js
@@ -10,6 +10,36 @@ const hiroshima = () => {
     <HiroshimaPage>
       <h1>Hiroshima before and after</h1>
       <ImageBeforeAndAfter before={hiroshimaBefore} after={hiroshimaAfter} />
+      <h2>pomnik 1</h2>
+      <div className="text">
+        Sadako W 1958 roku powstał pomnik Sadako Sasaki, japońskiej dziewczynki
+        która w wieku 2 lat przeżyła wybuch bobmy atomowej zrzuconej na
+        Hiroshime. Pomnik powstał z funduszy zebranych przez przyjaciół
+        dziewczyny jak i wielu innych młodych Japończyków. Owy pomnik stoi w
+        parku w Hiroshimie. Jest to ogromny trójnogi postument na którym
+        znajduje się rzeźba dziewczynki trzymająca nad głową ogromnego żurawia
+        origami. Na owym pomniku widnieje napis: "To jest nasz płacz, to jest
+        nasza modlitwa, pokój na świecie". Ludzie przynoszą pod pomnik papierowe
+        żurawie które są symbolem szczęścia oraz długowieczności.
+      </div>
+      <h2>pomnik 2</h2>
+      <div className="text">
+        Pomnik Pokoju W Hiroshimie mieszczą się ruiny centrum wystawowego
+        zachowane jako pomnik upamiętniający zrzucenie bomb na Hiroshime i
+        Nagasaki, należy do Parku Pamięci Pokoju w Hiroshimie. W 1996r. pomnik
+        został wpisany na liste światowego dziedzictwa UNESCO. Owy pomnik był
+        niegdyś centrum wystawowym zaprojektowanym w celu promocji przemysłu
+        prefektury Hiroshima przez czeskiego architekta. Pięciopiętrowy
+        secesyjny gmach z charakterystyczna 25-metrową kopułą stanał nad
+        brzegiem rzeki Motoyasu w ruchliwej dzielnicy handlowej. Wewnątrz
+        wystawiano obiekty związane z przemysłem, prace rzemieślników z różnych
+        regionów japonii a także dzieła sztuki. Eksplozja bomby miałą miejsce w
+        bezpośredniej bliskości hali. Hipocentrum znajdowało się ok 150m od
+        niej. Prawie wszystkie budynki w promieniu dwóch kilometrów od
+        hipocentrum zostały zrównane z ziemią. Pomnik Pokoju to jedyny budynek
+        który nie uległ całkowitemu zniszczeniu, Wskutek eksplozji zawalila się
+        większość ścian, a z kopuły ostał się jej żelazny szkielet.
+      </div>
     </HiroshimaPage>
   );
 };
@@ -20,6 +50,14 @@ const HiroshimaPage = styled.div`
   align-items: Center;
   flex-direction: column;
   text-align: center;
+  h2 {
+    padding: 1rem 0;
+    text-transform: upperCase;
+  }
+  .text {
+    width: 90%;
+    padding: 1rem 0;
+  }
 `;
 
 export default hiroshima;
--- src/pages/nagasaki.js
+++ src/pages/nagasaki.js
@@ -12,6 +12,37 @@ const nagasaki = () => {
     <NagasakiPage>
       <h1>Nagasaki before and after</h1>
       <ImageBeforeAndAfter before={nagasakiBefore} after={nagasakiAfter} />
+      <h2>pomnik1</h2>
+      <div className="text">
+        Pomnik "Kwiat życia i pokoju" Również znany pod nazwą Pomniku Ofiar
+        Nagasaki autorstwa Polaka Mariusza Kulpy, replika zrealizowanego pomnika
+        Dar Narodu Polskiego odsłoniętego w 1986 roku w Nagasaki,
+        upamiętniającego ofiary bomby atomowej z 1945 roku. Replika została
+        odsłonięta 9 VIII 1988 roku na skwerze między ulicami Szeroką i Św
+        Ducha, przedstawia metalową rzeźbę o wysokości 2m, w formie
+        skamieniałych liści symbolizujących zagłądę oraz wyrastającym nowym
+        kwiatem symbolem życia i odrodzenia. Początkowo przeznaczony do
+        Warszawy, ostatecznie stanął w Gdańku, portowym mieście, w którym 2WŚ
+        się rozpoczełą, zakończyła się natomiast w Nagasaki, innym portowym
+        mieście.
+      </div>
+      <h2>pomnik2</h2>
+      <div className="text">
+        Park Pokoju w Nagasaki` Założony w 1955 zlokalizowany w pobliżu
+        hipocentrum eksplozji, w parku znajduje się 10-metrowa statuła Pokoju. W
+        prawej ręce mierzy w zagrożenie bronią jądrową a lewą ręką w niekończący
+        się pokój. Łagodna twarz symbolizująca łaskę a delikatnie zamknięte oczy
+        modlą sięza spokój dusz ofiar bomby. Zgięta prawa noga i wyprostowana
+        lewa noga oznaczają zarówno medytację, jak i inicjatywę, aby powstać i
+        uratować ludzi na całym świecie. Co roku 9 Sierpnia, obchodzona jest
+        rocznica bombardowania, przed pomnikiem odbywa się ceremonia
+        upamiętniająca a burmistrz Nagasaki przekazuje światu deklaracje pokoju.
+        W 1978 Nagasaki stworzyło Strefe Symboli Pokoju po dwóch stronach parku
+        i zaprosili kraje z całęgo świata do dorzucenia swoich symboli. Takich
+        jak: Relief of Friendship z Porto, Portugali/Joy of Life z
+        Czechosłowacji/A Call z Bługarii/Flower of Love and Peace z Polski i
+        wiele innych. https://en.wikipedia.org/wiki/Nagasaki_Peace_Park
+      </div>
     </NagasakiPage>
   );
 };
@@ -22,6 +53,14 @@ const NagasakiPage = styled.div`
   align-items: Center;
   flex-direction: column;
   text-align: center;
+  h2 {
+    padding: 1rem 0;
+    text-transform: upperCase;
+  }
+  .text {
+    width: 90%;
+    padding: 1rem 0;
+  }
 `;
 
 export default nagasaki;
